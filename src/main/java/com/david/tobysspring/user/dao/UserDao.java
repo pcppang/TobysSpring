8c62f2d32ca18ed4530832decd5049b040d24898
David <david.chihyun.park@gmail.com>
2019-01-08 11:48:05 +0900

    [#13][REFACTOR] 클래스 인스턴스 변수 원상복구
    코드에 심각한 문제가 있다.
    멀티스레드 환경에서 값이 매번 변경되는 오브젝트를 클래스 인스턴스 변수로 사용할 경우
    값이 덮어써질 우려가 있다.
    다시 원상복구
    
    다만, ConnectionMaker는 읽기 전용이므로 클래스 인스턴스 변수를 활용한다.
    
    Resolves #13

--- src/main/java/com/david/tobysspring/user/dao/UserDao.java
+++ src/main/java/com/david/tobysspring/user/dao/UserDao.java
@@ -10,11 +10,6 @@ import com.david.tobysspring.user.domain.User;
 public class UserDao {
 	// 초기에 설정하면 사용 중에는 바뀌지 않는 읽기 전용 인스턴스 변수
 	private ConnectionMaker connectionMaker;
-	
-	// 매번 새로운 값으로 바뀌는 정보를 담은 인스턴스 변수
-	// 심각한 문제가 발생한다.
-	private Connection c;
-    private User user;
 
 	public UserDao(ConnectionMaker connectionMaker) {
 		this.connectionMaker = connectionMaker;
@@ -35,17 +30,17 @@ public class UserDao {
 	}
 	
 	public User get(String id) throws ClassNotFoundException, SQLException {
-		this.c = connectionMaker.makeConnection();
+		Connection c = connectionMaker.makeConnection();
 		
 		PreparedStatement ps = c.prepareStatement("SELECT * FROM USERS WHERE ID = ?");
 		ps.setString(1, id);
 		
 		ResultSet rs = ps.executeQuery();
 		rs.next();
-		this.user = new User();
-        this.user.setId(rs.getString("ID"));
-        this.user.setName(rs.getString("NAME"));
-        this.user.setPassword(rs.getString("PASSWORD"));
+		User user = new User();
+		user.setId(rs.getString("ID"));
+		user.setName(rs.getString("NAME"));
+		user.setPassword(rs.getString("PASSWORD"));
 		
 		rs.close();
 		ps.close();
