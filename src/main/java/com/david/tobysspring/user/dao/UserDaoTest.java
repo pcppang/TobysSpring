bf6586d8e76eee24be513af29c0e3e42cb78d12f
David <david.chihyun.park@gmail.com>
2019-01-10 15:44:31 +0900

    [#18][REFACTOR] 1.8.2 XML을 이용하는 애플리케이션 컨텍스트
    기존의 Java 코드로 되어있던 DaoFactory를 XML로 변경한다.
    XML은 작성과 관리가 용이하다.
    
    Resolves #18

--- src/main/java/com/david/tobysspring/user/dao/UserDaoTest.java
+++ src/main/java/com/david/tobysspring/user/dao/UserDaoTest.java
@@ -2,13 +2,15 @@ package com.david.tobysspring.user.dao;
 
 import java.sql.SQLException;
 
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 
 import com.david.tobysspring.user.domain.User;
 
 public class UserDaoTest {
 	public static void main(String[] args) throws ClassNotFoundException, SQLException{
-		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+		ApplicationContext context = new GenericXmlApplicationContext("/applicationContext.xml");
 		UserDao dao = context.getBean("userDao", UserDao.class);
 		
 		User user = new User();
