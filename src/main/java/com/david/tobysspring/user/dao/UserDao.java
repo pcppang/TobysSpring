a8ea886152e85645ef4ecdea6b071cf695715718
David <david.chihyun.park@gmail.com>
2019-01-10 16:27:25 +0900

    [#17]REFACTOR] 1.8.3 DataSource 인터페이스로 변환
    getConnection을 좀 더 풍부한 기능을 가진 DataSource 인터페이스로 변환
    불필요한 파일들을 삭제하고 나니 좀 더 깔끔한 코드로 변신했다.
    
    Resolves #17

--- src/main/java/com/david/tobysspring/user/dao/UserDao.java
+++ src/main/java/com/david/tobysspring/user/dao/UserDao.java
@@ -5,17 +5,19 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import javax.sql.DataSource;
+
 import com.david.tobysspring.user.domain.User;
 
 public class UserDao {
-	private ConnectionMaker connectionMaker;
-
-	public void setConnectionMaker(ConnectionMaker connectionMaker) {
-		this.connectionMaker = connectionMaker;
+	private DataSource dataSource;
+	
+	public void setDataSource(DataSource dataSource) {
+		this.dataSource = dataSource;
 	}
 
 	public void add(User user) throws ClassNotFoundException, SQLException {
-		Connection c = connectionMaker.makeConnection();
+		Connection c = dataSource.getConnection();
 		
 		PreparedStatement ps = c.prepareStatement("INSERT INTO USERS(ID, NAME, PASSWORD) VALUES (?, ?, ?)");
 		ps.setString(1, user.getId());
@@ -29,7 +31,7 @@ public class UserDao {
 	}
 	
 	public User get(String id) throws ClassNotFoundException, SQLException {
-		Connection c = connectionMaker.makeConnection();
+		Connection c = dataSource.getConnection();
 		
 		PreparedStatement ps = c.prepareStatement("SELECT * FROM USERS WHERE ID = ?");
 		ps.setString(1, id);
