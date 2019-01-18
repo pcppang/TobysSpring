b62eec73ce880164d6a71a6f53ee3d43f0aee68c
pi88@z0za <david.chihyun.park@gmail.com>
2019-01-18 16:42:42 +0900

    [#45][REFACTOR] add() 메소드에 전략패턴 적용
    add() 메소드에도 전략 패턴을 적용해주자
    
    add() 메소드에는 user 정보가 있어야하므로 생성자를 통해 넘겨주도록 하자
    
    Resolves #45

--- src/main/java/com/david/tobysspring/user/dao/UserDao.java
+++ src/main/java/com/david/tobysspring/user/dao/UserDao.java
@@ -17,19 +17,37 @@ public class UserDao {
 	public void setDataSource(DataSource dataSource) {
 		this.dataSource = dataSource;
 	}
+	
+	public void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException {
+		Connection c = null;
+		PreparedStatement ps = null;
+		
+		try { 
+			c = dataSource.getConnection();
+			ps = stmt.makePreparedStatement(c);			
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			throw e;
+		} finally {
+			if (ps != null) {
+				try {
+					ps.close();
+				} catch (SQLException e) {
+				}
+			}
+			
+			if (c != null) {
+				try {
+					c.close();
+				} catch (SQLException e) {
+				}			
+			}
+		}		
+	}
 
 	public void add(User user) throws SQLException {
-		Connection c = dataSource.getConnection();
-		
-		PreparedStatement ps = c.prepareStatement("INSERT INTO users(id, name, password) VALUES (?, ?, ?)");
-		ps.setString(1, user.getId());
-		ps.setString(2, user.getName());
-		ps.setString(3, user.getPassword());
-		
-		ps.executeUpdate();
-		
-		ps.close();
-		c.close();
+		StatementStrategy st = new AddAllStatement(user);
+		jdbcContextWithStatementStrategy(st);
 	}
 	
 	public User get(String id) throws SQLException {
@@ -65,33 +83,8 @@ public class UserDao {
 	}
 	
 	public void deleteAll() throws SQLException {
-		Connection c = null;
-		PreparedStatement ps = null;
-		
-		try { 
-			c = dataSource.getConnection();
-			
-			StatementStrategy strategy = new DeleteAllStatement();
-			ps = strategy.makePreparedStatement(c);
-			
-			ps.executeUpdate();
-		} catch (SQLException e) {
-			throw e;
-		} finally {
-			if (ps != null) {
-				try {
-					ps.close();
-				} catch (SQLException e) {
-				}
-			}
-			
-			if (c != null) {
-				try {
-					c.close();
-				} catch (SQLException e) {
-				}			
-			}
-		}
+		StatementStrategy st = new DeleteAllStatement();
+		jdbcContextWithStatementStrategy(st);
 	}
 	
 	public int getCount() throws SQLException{
