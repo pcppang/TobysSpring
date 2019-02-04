1e56d8a30b6cb300d5e62fd9730b4263d29c5bff
pi88@z0za <david.chihyun.park@gmail.com>
2019-02-04 18:05:41 +0900

    [#79][REFACTOR] 테스트 코드 리팩토링
    테스트 코드 내에 중복이 발견되어 리팩토링을 함
    
    1. 다음 단계가 무엇인지 직접 넣어주는 중복을 제거
    2. 로그인 횟수와 추천 횟수의 중복을 제거
    
    Resoloves #79

--- src/main/java/com/david/tobysspring/user/service/UserService.java
+++ src/main/java/com/david/tobysspring/user/service/UserService.java
@@ -9,6 +9,9 @@ import com.david.tobysspring.user.domain.User;
 public class UserService {
 	UserDao userDao;
 	
+	public static final int MIN_LOGCOUNT_FOR_SILVER = 50;
+	public static final int MIN_RECOMMEND_FOR_GOLD = 30;
+	
 	public void setUserDao(UserDao userDao) {
 		this.userDao = userDao;
 	}
@@ -32,8 +35,8 @@ public class UserService {
 	private boolean canUpgradeLvl(User user) {
 		Lvl currentLvl = user.getLvl();
 		switch(currentLvl) {
-			case BASIC: return (user.getLogin() >= 50);
-			case SILVER: return (user.getRecommend() >= 30);
+			case BASIC: return (user.getLogin() >= MIN_LOGCOUNT_FOR_SILVER);
+			case SILVER: return (user.getRecommend() >= MIN_RECOMMEND_FOR_GOLD);
 			case GOLD: return false;
 			default: throw new IllegalArgumentException("Unknown Level : " + currentLvl);
 		}
--- src/main/java/com/david/tobysspring/user/service/UserServiceTest.java
+++ src/main/java/com/david/tobysspring/user/service/UserServiceTest.java
@@ -1,5 +1,8 @@
 package com.david.tobysspring.user.service;
 
+import static com.david.tobysspring.user.service.UserService.MIN_LOGCOUNT_FOR_SILVER;
+import static com.david.tobysspring.user.service.UserService.MIN_RECOMMEND_FOR_GOLD;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -36,10 +39,10 @@ public class UserServiceTest {
 		((UserDaoJdbc) userDao).setDataSource(dataSource);
 		
 		users = Arrays.asList(
-			new User("bumjin", "박범진", "p1", Lvl.BASIC, 49, 0),
-			new User("joytouch", "강명성", "p2", Lvl.BASIC, 50, 0),
-			new User("erwins", "신승한", "p3", Lvl.SILVER, 60, 29),
-			new User("madnite1", "이상호", "p4", Lvl.SILVER, 60, 30),
+			new User("bumjin", "박범진", "p1", Lvl.BASIC, MIN_LOGCOUNT_FOR_SILVER-1, 0),
+			new User("joytouch", "강명성", "p2", Lvl.BASIC, MIN_LOGCOUNT_FOR_SILVER, 0),
+			new User("erwins", "신승한", "p3", Lvl.SILVER, 60, MIN_RECOMMEND_FOR_GOLD-1),
+			new User("madnite1", "이상호", "p4", Lvl.SILVER, 60, MIN_RECOMMEND_FOR_GOLD),
 			new User("green", "오민", "p5", Lvl.GOLD, 100, 100)
 		);
 	}
@@ -54,13 +57,13 @@ public class UserServiceTest {
 		
 		userService.upgradeLvls();
 		
-		checkLvl(users.get(0), Lvl.BASIC);
-		checkLvl(users.get(1), Lvl.SILVER);
-		checkLvl(users.get(2), Lvl.SILVER);
-		checkLvl(users.get(3), Lvl.GOLD);
-		checkLvl(users.get(4), Lvl.GOLD);
+		checkLvlUpgraded(users.get(0), false);
+		checkLvlUpgraded(users.get(1), true);
+		checkLvlUpgraded(users.get(2), false);
+		checkLvlUpgraded(users.get(3), true);
+		checkLvlUpgraded(users.get(4), false);
 	}
-	
+
 	@Test
 	public void add() {
 		userDao.deleteAll();
@@ -82,5 +85,14 @@ public class UserServiceTest {
 	private void checkLvl(User user, Lvl expectedLvl) {
 		User userUpdate = userDao.get(user.getId());
 		assertThat(userUpdate.getLvl(), is(expectedLvl));
+	}
+	
+	private void checkLvlUpgraded(User user, boolean upgraded) {
+		User userUpdate = userDao.get(user.getId());
+		if (upgraded) {
+			assertThat(userUpdate.getLvl(), is(user.getLvl().nextLvl()));
+		} else {
+			assertThat(userUpdate.getLvl(), is(user.getLvl()));
+		}
 	}
 }
