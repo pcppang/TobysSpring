c156c887cfd6c85c7894d75be9788ff2b9ace1ad
pi88@z0za <david.chihyun.park@gmail.com>
2019-02-06 16:51:15 +0900

    [#81][UPDATE] 글로벌 트랜잭션 적용
    여러 개의 DB에 대한 작업을 하나의 트랜잭션으로 묶기 위해서는 글로벌 트랜잭션을 사용하여야 한다.
    
    Resolves #81

--- src/main/java/com/david/tobysspring/user/service/UserService.java
+++ src/main/java/com/david/tobysspring/user/service/UserService.java
@@ -1,12 +1,13 @@
 package com.david.tobysspring.user.service;
 
-import java.sql.Connection;
 import java.util.List;
 
 import javax.sql.DataSource;
 
-import org.springframework.jdbc.datasource.DataSourceUtils;
-import org.springframework.transaction.support.TransactionSynchronizationManager;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import com.david.tobysspring.user.dao.UserDao;
 import com.david.tobysspring.user.domain.Lvl;
@@ -35,10 +36,9 @@ public class UserService {
 	}
 	
 	public void upgradeLvls() throws Exception {
-		TransactionSynchronizationManager.initSynchronization();
-		Connection c = DataSourceUtils.getConnection(dataSource);
-		c.setAutoCommit(false);
-		
+		PlatformTransactionManager transactionManager = new DataSourceTransactionManager(dataSource);
+		TransactionStatus status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+
 		try {
 			List<User> users = userDao.getAll();
 			for (User user : users) {
@@ -46,16 +46,11 @@ public class UserService {
 					upgradeLvl(user);
 				}
 			}
-			
-			c.commit();
+			transactionManager.commit(status);
 		} catch (Exception e) {
-			c.rollback();
+			transactionManager.rollback(status);
 			throw e;
-		} finally {
-			DataSourceUtils.releaseConnection(c, dataSource);
-			TransactionSynchronizationManager.unbindResource(this.dataSource);
-			TransactionSynchronizationManager.clearSynchronization();
-		}
+		} 
 	}
 
 	private boolean canUpgradeLvl(User user) {
