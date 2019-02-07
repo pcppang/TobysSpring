ecf2619e6d4f874b8c14e052b32723af9e2b88c6
pi88@z0za <david.chihyun.park@gmail.com>
2019-02-07 12:46:39 +0900

    [#84][ADD] JavaMail을 이용한 메일 발송 기능 추가 (테스트 진행 안 됨)
    JavaMail을 이용한 메일 발송 기능 추가
    
    현재 메일 서버가 없으므로 테스트 진행 안 됨
    메일 서버가 있다면 진행될 것으로 믿고 (믿지마라)
    
    Resolves #84

--- src/main/java/com/david/tobysspring/user/service/UserService.java
+++ src/main/java/com/david/tobysspring/user/service/UserService.java
@@ -1,6 +1,15 @@
 package com.david.tobysspring.user.service;
 
 import java.util.List;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
@@ -62,5 +71,26 @@ public class UserService {
 	protected void upgradeLvl(User user) {
 		user.upgradeLvl();
 		userDao.update(user);
+		sendUpgradeEmial(user);
+	}
+
+	private void sendUpgradeEmial(User user) {
+		Properties props = new Properties();
+		props.put("mail.smtp.host", "mail.ksug.org");
+		Session s = Session.getInstance(props, null);
+		
+		MimeMessage message = new MimeMessage(s);
+		try {
+			message.setFrom(new InternetAddress("useradmin@ksug.org"));
+			message.setRecipient(Message.RecipientType.TO, new InternetAddress(user.getEmail()));
+			message.setSubject("Upgrade 안내");
+			message.setText("사용자님의 등급이 " + user.getLvl().name() + "로 업그레이드되었습니다.");
+			
+			Transport.send(message);
+		} catch (AddressException e) {
+			throw new RuntimeException(e);
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
 	}
 }
