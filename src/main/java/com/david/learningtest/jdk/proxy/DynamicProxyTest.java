25e219b7bf69a1fd498f605aada8077943b2f80a
pi88@z0za <david.chihyun.park@gmail.com>
2019-02-13 18:09:02 +0900

    [#101][ADD] 확장 포인트컷 테스트
    빈 후처리기 등록 전 확장 포인트컷에 대한 학습테스트 추가
    
    Resolves #101

--- src/main/java/com/david/learningtest/jdk/proxy/DynamicProxyTest.java
+++ src/main/java/com/david/learningtest/jdk/proxy/DynamicProxyTest.java
@@ -6,11 +6,16 @@ import static org.junit.Assert.assertThat;
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
 import org.junit.Test;
+import org.springframework.aop.ClassFilter;
+import org.springframework.aop.Pointcut;
 import org.springframework.aop.framework.ProxyFactoryBean;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
 import org.springframework.aop.support.NameMatchMethodPointcut;
 
 public class DynamicProxyTest {
+	/**
+	 * 프록시 팩토리 빈 테스트
+	 */
 	@Test
 	public void proxyFactoryBean() {
 		ProxyFactoryBean pfBean = new ProxyFactoryBean();
@@ -23,6 +28,9 @@ public class DynamicProxyTest {
 		assertThat(proxiedHello.sayThankYou("Toby"), is("THANK YOU TOBY"));
 	}
 	
+	/**
+	 * 포인트컷 어드바이저 테스트
+	 */
 	@Test
 	public void pointcutAdvisor() {
 		ProxyFactoryBean pfBean = new ProxyFactoryBean();
@@ -40,6 +48,52 @@ public class DynamicProxyTest {
 		assertThat(proxiedHello.sayThankYou("Toby"), is("Thank You Toby"));
 	}
 	
+	/**
+	 * 확장 포인트컷 테스트
+	 */
+	@Test
+	public void classNamePointcutAdvisor() {
+		// 포인트컷 준비
+		NameMatchMethodPointcut classMethodPointcut = new NameMatchMethodPointcut() {
+			private static final long serialVersionUID = 1L;
+
+			public ClassFilter getClassFilter() {
+				return new ClassFilter() {
+					public boolean matches(Class<?> clazz) {
+						return clazz.getSimpleName().startsWith("HelloT");
+					}
+				};
+			}
+		};
+		classMethodPointcut.setMappedName("sayH*");
+		
+		// 테스트
+		checkAdviced(new HelloTarget(), classMethodPointcut, true); // 적용 클래스
+		
+		class HelloWorld extends HelloTarget {};
+		checkAdviced(new HelloWorld(), classMethodPointcut, false); // 적용 클래스 아님
+		
+		class HelloToby extends HelloTarget {};
+		checkAdviced(new HelloToby(), classMethodPointcut, true); // 적용 클래스
+	}
+	
+	private void checkAdviced(Object target, Pointcut pointcut, boolean adviced) {
+		ProxyFactoryBean pfBean = new ProxyFactoryBean();
+		pfBean.setTarget(target);
+		pfBean.addAdvisor(new DefaultPointcutAdvisor(pointcut, new UppercaseAdvice()));
+		Hello proxiedHello = (Hello) pfBean.getObject();
+		
+		if (adviced) {
+			assertThat(proxiedHello.sayHello("Toby"), is("HELLO TOBY"));
+			assertThat(proxiedHello.sayHi("Toby"), is("HI TOBY"));
+			assertThat(proxiedHello.sayThankYou("Toby"), is("Thank You Toby"));
+		} else {
+			assertThat(proxiedHello.sayHello("Toby"), is("Hello Toby"));
+			assertThat(proxiedHello.sayHi("Toby"), is("Hi Toby"));
+			assertThat(proxiedHello.sayThankYou("Toby"), is("Thank You Toby"));
+		}
+	}
+
 	static class UppercaseAdvice implements MethodInterceptor {
 		@Override
 		public Object invoke(MethodInvocation invocation) throws Throwable {
